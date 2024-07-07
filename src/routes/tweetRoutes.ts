775a84dc78cd4664a05c25e2a7ed606ccfb7761d
Daniel Britto <daniebritto83@gmail.com>
2024-07-07 17:27:59 +0530

    tweetsCrud

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,41 +1,75 @@
-import express  from "express"
-
-const router =  express.Router()
-
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+const router = express.Router();
+const prisma = new PrismaClient();
 
 //create tweets
-router.post('/',(req,res)=>{
-    res.status(501).json({error:'Not Implemented'})
-})
+router.post("/", async (req, res) => {
+    const { content, image, userId } = req.body;
+    console.log(content, image, userId);
+    try {
+        const result = await prisma.tweet.create({
+            data: {
+                content,
+                image,
+                userId, //TODO manage based on the auth user
+            },
+        });
 
+        res.status(201).json(result);
+    } catch (err) {
+        res.status(400).json({ error: "couldnt post tweeet" });
+    }
+});
 
 //list tweets
-router.get('/',(req,res)=>{
-    res.status(501).json({error:"Not Implemented"})
-})
+router.get("/", async (req, res) => {
+    const allTweets = await prisma.tweet.findMany({include:{user:{select:{email:true,username:true}}}});
+    res.status(200).json(allTweets);
+});
 
 //get one tweets
-router.get('/:id',(req,res)=>{
-    const {id} =req.params;  
-     
-    res.status(501).json({error:`Not implemented : ${id}`})
-    
-})
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+    if (!tweet) {
+        res.status(404).json({ message: "No tweet found " });
+    }
+    res.json(tweet);
+});
 
 //update tweets
-router.get('/:id',(req,res)=>{
-    const {id} =req.params;  
-     
-    res.status(501).json({error:`Not implemented : ${id}`})
-    
-})
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const { content, image, userId } = req.body;
+
+    try {
+        const result = await prisma.tweet.update({
+            where: { id: Number(id) },
+            data: {
+                content,
+                image,
+                userId,
+            },
+        });
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ error: `Failed to update user${id}` });
+    }
+});
 
 //delete tweets
-router.delete('/:id',(req,res)=>{
-    const {id} =req.params;  
-     
-    res.status(501).json({error:`Not implemented : ${id}`})
-    
-})
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
 
-export default router
+    await prisma.tweet.delete({
+        where: {
+            id: Number(id),
+        },
+    });
+
+    res.status(409).json({ message: `Tweet Deleted Successfully` });
+});
+
+export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,77 +1,79 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
-import { error } from "console";
+
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
 //create user
 router.post("/", async (req, res) => {
-  const { email, name, username } = req.body;
-  console.log(email, name, username);
-  try {
-    const result = await prisma.user.create({
-      data: {
-        email,
-        name,
-        username,
-        bio: "Hello Im new to Twitter",
-      },
-    });
+    const { email, name, username } = req.body;
+    console.log(email, name, username);
+    try {
+        const result = await prisma.user.create({
+            data: {
+                email,
+                name,
+                username,
+                bio: "Hello Im new to Twitter",
+            },
+        });
 
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(400).json({ error: "Username and Email should be unique" });
-  }
+        res.status(201).json(result);
+    } catch (err) {
+        res.status(400).json({ error: "Username and Email should be unique" });
+    }
 });
 
 //list user
 
 router.get("/", async (req, res) => {
-  const allUser = await prisma.user.findMany();
-  res.status(200).json(allUser);
+    const allUser = await prisma.user.findMany({ select: { id: true, image: true, name: true } });
+    res.status(200).json(allUser);
 });
 
 //get one user
 
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    const user = await prisma.user.findUnique({ where: { id: Number(id) }, include: { tweet: true } });
 
-  res.json(user);
+    res.json(user);
 });
 
 //update user
 router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { bio, name, image } = req.body;
+    const { id } = req.params;
+    const { bio, name, image } = req.body;
 
-  try {
-    const result = await prisma.user.update({
-      where: { id: Number(id) },
-      data: {
-        bio,
-        image,
-        name,
-      },
-    });
-    res.json(result);
-  } catch (err) {
-    res.status(400).json({ error: `Failed to update user${id}` });
-  }
+    try {
+        const result = await prisma.user.update({
+            where: { id: Number(id) },
+            data: {
+                bio,
+                image,
+                name,
+            },
+        });
+        res.json(result);
+    } catch (err) {
+        res.status(400).json({ error: `Failed to update user${id}` });
+    }
 });
 
 //delete user
 
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  await prisma.user.delete({where:{
-    id:Number(id)
-  }})
+    await prisma.user.delete({
+        where: {
+            id: Number(id)
+        }
+    })
 
-  res.status(409).json({ message: `User Deleted Successfully` });
+    res.status(409).json({ message: `User Deleted Successfully` });
 });
 
 export default router;
